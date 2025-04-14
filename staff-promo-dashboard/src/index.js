b3028a5e39e5524d53242a03ca1e96d29db8469c
kxthu <patelkathu@gmail.com>
2025-04-14 12:07:51 -0400

    Delete staff-promo-dashboard directory

--- staff-promo-dashboard/src/index.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import "./index.css";
-import StaffPromoDashboard from "./StaffPromoDashboard";
-
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<StaffPromoDashboard />);
